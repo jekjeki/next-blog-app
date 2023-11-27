055045b99757400ef2bc4334f364850308ca7c1c
jekjeki <yusufzaki013@gmail.com>
2023-11-27 09:07:37 +0700

    final-2

--- next.config.js
+++ next.config.js
@@ -1,13 +1,37 @@
 /** @type {import('next').NextConfig} */
 
+const {PHASE_DEVELOPMENT_SERVER} = require('next/constants')
+
 const nextConfig = {
   reactStrictMode: true,
   env: {
     mongodb_username: 'zaki',
     mongodb_password: 'zaki123',
     mongodb_cluster: 'cluster0',
-    mo
+    
   }
 }
 
-module.exports = nextConfig
+module.exports = (phase) => {
+  if(phase == PHASE_DEVELOPMENT_SERVER){
+    return {
+      reactStrictMode: true,
+      env: {
+        mongodb_username: 'zaki',
+        mongodb_password: 'zaki123',
+        mongodb_cluster: 'cluster0',
+        
+      }
+    }
+  }
+
+  return {
+    reactStrictMode: true,
+      env: {
+        mongodb_username: 'zaki',
+        mongodb_password: 'zaki123',
+        mongodb_cluster: 'cluster0',
+        
+      }
+  }
+}
